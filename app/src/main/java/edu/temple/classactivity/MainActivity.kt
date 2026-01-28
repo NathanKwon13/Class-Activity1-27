bf584d1daceeeb0c0c00824f77da262c45548a00
nathankwon <nathankwon813@gmail.com>
2026-01-28 10:05:16 -0500

    Built Buttons and CheckBox with EventListeners that impacted a TextView.

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -6,9 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+// different views used
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.textView)
+        val btnLeft: Button = findViewById(R.id.btnLeft)
+        val btnRight: Button = findViewById(R.id.btnRight)
+        val enabler: CheckBox = findViewById(R.id.enabler)
+
+        btnLeft.setOnClickListener {
+            if (enabler.isChecked) {
+                text.text = "Left"
+            }
+        }
+
+        btnRight.setOnClickListener {
+            if (enabler.isChecked) {
+                text.text = "Right"
+            }
+        }
     }
 }
